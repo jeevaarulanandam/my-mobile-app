1464b9878c89e31c3593d96347b1d8f115838e28
Jeeva Arulanandam <jeevaarulanandam@gmail.com>
2017-12-01 05:41:06 -0500

    changes added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component,ViewChild} from '@angular/core';
+import { Slides, NavController, AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+@ViewChild(Slides)
+  slides: Slides;
 
-  constructor(public navCtrl: NavController) {
+
+  sampleFunction(){
+
+    console.log("Extends Works");
+  }
+
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Hi',
+      subTitle: 'You Reached Last Slide',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  slideChanged(){
+    let currentIndex = this.slides.getActiveIndex();
+ 
+    console.log("current index is"+currentIndex);
+
+     if (this.slides.isEnd()){
+      this.showAlert();
+    }
+  }
+
+constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+   
+    
+
 
   }
 
 }
+
